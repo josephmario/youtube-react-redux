f70ced57c269f695eaec13f365f3f479a685f8d1
seph <josephmario_santiago@yahoo.com>
2016-10-20 15:23:40 +0800

    search video youtube

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,9 +7,25 @@ import React, { Component } from 'react';
 // }
 
 class SearchBar extends Component{
-  render(){
-    return <input />;
+  constructor(props){
+    super(props);
+    //this.state = object
+    this.state = { term: ''};
   }
+  render(){
+    return(
+    <div>
+      <input
+        value={this.state.term}
+        onChange={event => this.setState({ term: event.target.value })}/>
+        Value of the input: {this.state.term}
+    </div>
+    );
+  }
+  // onInputChange(event){
+  //   //checking if onChange called or get the value of textbox
+  //   console.log(event.target.input)
+  // }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const KEY_API = 'AIzaSyCV9mS9q_iCdNsG62buQn0uoZFsHw5U49c';
 
+YTSearch({ key: KEY_API, term: 'react.js'}, function(data){
+  console.log(data)
+});
+
 // const App = function(){
+
 const App = () => {
-  return <div> hello world </div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
